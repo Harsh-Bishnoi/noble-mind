2a01d395f154bfac90cc5a637de484f45614a7a8
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-02 20:28:58 +0530

    add back to top function

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -146,3 +146,22 @@ container.innerHTML = cardsData.map(card => `
             </div>
         `)
     .join('');
+
+
+
+    
+// back to top
+let backToTopButton = document.getElementById("backToTop");
+window.onscroll = function () {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+backToTopButton.onclick = function () {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
