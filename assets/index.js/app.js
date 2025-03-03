6754c9bf1ff9a2812acccae27803e3d22c145eed
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-03-03 14:25:06 +0530

    make JOIN COMPANIES section slider on mobile screen

--- assets/index.js/app.js
+++ assets/index.js/app.js
@@ -249,3 +249,36 @@ backToTopButton.onclick = function () {
         behavior: 'smooth'
     });
 };
+
+
+
+
+// JOIN COMPANIES SLIDER
+$('.autoplay').slick({
+    autoplay: true,
+    autoplaySpeed: 900,
+    slidesToShow: 3,
+    arrows: false,
+    dots: false,
+    pauseOnMouseEnter: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+            }
+        }
+    ]
+});
